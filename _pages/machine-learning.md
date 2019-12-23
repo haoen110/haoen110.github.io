ebe004d893358cbe2f54e4c07eef2aa84302ddc3
haoen110 <haoenwei@outlook.com>
2019-12-23 15:20:01 +0800

    mm

---
+++ _pages/machine-learning.md
@@ -0,0 +1,8 @@
+---
+layout: archive
+permalink: /machine-learning/
+title: "Machine Learning"
+author_profile: true
+header: 
+    image: "/assets/images/ml.jpg"
+---
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,11 @@
 var store = [{
-        "title": "Edge Case: Nested and Mixed Lists",
-        "excerpt":"First  hahah  ","categories": ["Coding"],
+        "title": "First Day!",
+        "excerpt":"   This is my first day deploying a blog website on github pages.    ","categories": ["Article"],
         "tags": [],
-        "url": "http://localhost:4000/blogs/coding/first/",
+        "url": "http://localhost:4000/article/first/",
+        "teaser":null},{
+        "title": "Gradient Descent Intro",
+        "excerpt":"Gradient Descent Today, I’m going to try this method to solve a linear regression problem. Function can be written as: The cost function, “Squared error function”, or “Mean squared error” is: Iterate until function $J(\\theta)$ to Iterate by: ”:=” means renew the value. Import import numpy as np import pandas...","categories": ["machine-learning"],
+        "tags": ["machine learning","data science","linear regression"],
+        "url": "http://localhost:4000/machine-learning/gradient-descent/",
         "teaser":null}]
