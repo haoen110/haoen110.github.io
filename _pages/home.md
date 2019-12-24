563e51d1c050dca9ac855f70f892e905403674ea
haoen110 <haoenwei@outlook.com>
2019-12-24 13:57:21 +0800

    m

---
+++ _pages/home.md
@@ -0,0 +1,8 @@
+---
+title: "Home"
+layout: home
+permalink: /
+author_profile: true
+date: 2019-12-24
+---
+
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,6 @@
 var store = [{
-        "title": "First Day!",
-        "excerpt":"   This is my first day deploying a blog website on github pages.    ","categories": ["Article"],
+        "title": "First Day Post Example",
+        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/article/first/",
-        "teaser":null},{
-        "title": "Gradient Descent Intro",
-        "excerpt":"Gradient Descent Today, I’m going to try this method to solve a linear regression problem. Function can be written as: The cost function, “Squared error function”, or “Mean squared error” is: Iterate until function $J(\\theta)$ to Iterate by: ”:=” means renew the value. Import import numpy as np import pandas...","categories": ["machine-learning"],
-        "tags": ["machine learning","data science","linear regression"],
-        "url": "http://localhost:4000/machine-learning/gradient-descent/",
+        "url": "http://localhost:4000/first-day/",
         "teaser":null}]
